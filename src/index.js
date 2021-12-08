8512bc1de9a9b7dd938465f0a22df13bcc5c9063
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-08 10:00:40 +0100

    s

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const movies = require('./data/movies.json');
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
+
+    return fetchWithTimeout(1000).then(resolveFunction);
 } 
