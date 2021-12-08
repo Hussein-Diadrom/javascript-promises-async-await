ddb79db2f7a3e2699f20fd0762468039b31b58e1
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-08 11:25:23 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import {fetchWithTimeout } from './services'; 
+import {fetchWithTimeout,fetchBooks,fetchMovies } from './services'; 
 const movies = require('./data/movies.json');
 
+
+function getBooksAndMovies(){
+    Promise.all([fetchBooks(),fetchMovies()]).then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
+}
